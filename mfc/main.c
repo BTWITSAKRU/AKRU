9ccfcc0a2064ea542232b9f4022112e27cd43472
BTWITSAKRU <Akramcei78@gmail.com>
2024-05-17 08:28:16 +0000

    adding 2 numbers

---
+++ mfc/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2;
+    int res;
+    printf("enter 2 numbers");
+    scanf("%d %d",&num1,&num2);
+    res=num1+num2;
+    printf("result %d",res);
+}
