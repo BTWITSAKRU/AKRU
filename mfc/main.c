af1fbe9cb29c3d56230803fb2cf3326f208de6a1
BTWITSAKRU <Akramcei78@gmail.com>
2024-05-17 08:30:08 +0000

    adding of 3 numbers

--- mfc/main.c
+++ mfc/main.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-    int num1,num2;
+    int num1,num2,num3;
     int res;
-    printf("enter 2 numbers");
-    scanf("%d %d",&num1,&num2);
-    res=num1+num2;
+    printf("enter 3 numbers");
+    scanf("%d %d %d",&num1,&num2,&num3);
+    res=num1+num2+num3;
     printf("result %d",res);
 }
